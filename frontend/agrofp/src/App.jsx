275f4cfa9e8cb818e826a5b745d8872585cb2475
Tisha <159291720+Itz-Tisha@users.noreply.github.com>
2025-10-31 17:39:18 +0530

    Refactor App component routes and imports

--- frontend/agrofp/src/App.jsx
+++ frontend/agrofp/src/App.jsx
@@ -14,53 +14,49 @@ import Expert from './components/Expert';
 import Postq from './components/Postq';
 import Userprofile from './components/Userprofile';
 import Articles from './components/Articles';
-//import Displayq from './components/displayq';
 import Displayq from './components/Displayq';
-
 import Postart from './components/Postart';
 import Answerq from './components/Answerq';
 import Viewanswer from './components/Viewanswer';
 import Editprofile from './components/Editprofile';
-
 import Viewweather from './components/Viewweather';
 import Comments from './components/Comments';
-
 import Notification from './components/Notification';
-
 import Askai from './components/Askai';
 import Trending_art from './components/Trending_art';
+import Diseasedetect from './components/Diseasedetect';
 function App() {
 
-  return (
-    <>
-  
-      <Routes>
+ return (
+  <>
+   
+
+    <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/sign" element={<Sign />} />
-      <Route path="/farmerpage" element={<Farmer/>} />
-      <Route path="/expertpage" element={<Expert/>} />
-      <Route path="/postq" element={<Postq/>} />
-      <Route path="/userprofile" element={<Userprofile/>} />
-      <Route path="/articles" element={<Articles/>} />
-      <Route path="/Displayq" element={<Displayq/>}/>
-      <Route path="/postart" element={<Postart/>}/>
-      <Route path ="/viewweather" element={<Viewweather/>}/>
-      <Route path ="/Answerq" element={<Answerq/>}/>
-      <Route path ="/viewans" element={<Viewanswer/>}/>
-      <Route path="/editprofile" element={<Editprofile/>}/>
-      
+      <Route path="/farmerpage" element={<Farmer />} />
+      <Route path="/expertpage" element={<Expert />} />
+      <Route path="/postq" element={<Postq />} />
+      <Route path="/userprofile" element={<Userprofile />} />
+      <Route path="/articles" element={<Articles />} />
+      <Route path="/Displayq" element={<Displayq />} />
+      <Route path="/postart" element={<Postart />} />
       <Route path="/viewweather" element={<Viewweather />} />
+      <Route path="/Answerq" element={<Answerq />} />
+      <Route path="/viewans" element={<Viewanswer />} />
+      <Route path="/editprofile" element={<Editprofile />} />
       <Route path="/comments" element={<Comments />} />
-      <Route path="/notification" element={<Notification/>} />
-     
-     <Route path="/askai" element={<Askai />} />
+      <Route path="/notification" element={<Notification />} />
+      <Route path="/askai" element={<Askai />} />
       <Route path="/trending_art" element={<Trending_art />} />
-     </Routes>  
+      <Route path="/DiseaseDetection" element={<Diseasedetect />} />
+      
+    </Routes>
+  </>
+)
 
-    
-    </>
-  )
 }
+
 
 export default App

b9fe47aaecc1f6ac8c2dbfc84758672808ed2f5d
Tisha <159291720+Itz-Tisha@users.noreply.github.com>
2025-11-17 22:38:42 +0530

    Update Expertheader.jsx

--- frontend/agrofp/src/components/Expertheader.jsx
+++ frontend/agrofp/src/components/Expertheader.jsx
@@ -1,87 +1,4 @@
 
-// import React, { useEffect ,useState} from 'react'
-// import { Link } from 'react-router-dom'
-// import { useNavigate } from 'react-router-dom'
-// import '../assets/Navbar.css'
-// import GoogleTranslate from './Googletranslate';
-// const Expertheader = () => {
- 
-//   const navigate = useNavigate();
-//     const [user, setUser] = useState('');
-//     const [usertype, setUsertype] = useState('');
-//     const [islogin, setIslogin] = useState(false);
-//     const [isfarmer, setIsfarmer] = useState(false);
-//      function logout() {
-      
-//       localStorage.removeItem('user');
-//       localStorage.removeItem('usertype');
-//       localStorage.removeItem('islogin');
-//       setUser('');
-//       setUsertype('');
-//       setIslogin(false);
-//       setIsfarmer(false);
-//       navigate('/');
-//     }
-//     useEffect(() => {
-//         const storedUser = localStorage.getItem('user');
-//         const storedUsertype = localStorage.getItem('usertype');
-//         const storedIslogin = localStorage.getItem('islogin');
-    
-//         if (storedUser) {
-//           setUser(JSON.parse(storedUser));
-//         }
-//         if (storedUsertype) {
-//           setUsertype(storedUsertype);
-//           if (storedUsertype === 'farmer') {
-//             setIsfarmer(true);
-//           }
-//         }
-//         if (storedIslogin) {
-//           setIslogin(storedIslogin === 'true');
-//         }
-//       }, []);
-//     return (
-//       <>
-
-//       <nav className="navbar">
-//             <div className="logo">
-//               <span>AgroConnect</span> <span className="emoji">🌱</span>
-//             </div>
-//             <div className="nav-links">
-//               <Link to="/">Home</Link>
-//               {islogin ? (
-//                 <>
-//                    <Link to="/displayq">Responses</Link>
-//                   <Link to="/postart"> Publish Wisdom</Link>
-//                   <Link to="/articles">Expert Insights</Link>
-                 
-//                   <Link to="/trending_art">Trending Articles</Link>
-//                   <Link to="/userprofile">Profile</Link>
-//                    <GoogleTranslate />
-
-//                   <button className="logout-btn" onClick={logout}>Logout</button>
-//                 </>
-                
-//               ) : (
-//                 <>
-//                  <Link to="/articles">Expert Insights</Link>
-//                   <Link to="/trending_art">Trending Articles</Link>
-//                    <div id="google_translate_element" className="translate-box"></div>
-//                   <Link to="/login">Login</Link>
-//                   <Link to="/sign">Sign Up</Link>
-//                 </>
-//               )}
-//             </div>
-
-           
-//           </nav>
-       
-   
-//       </>
-//     )
-// }
-
-// export default Expertheader
 
 
 import React, { useEffect, useState } from 'react';
@@ -130,7 +47,7 @@ const Expertheader = () => {
 
     loadAuthData();
 
-    // Optional: Reload auth state when language changes (due to Google Translate)
+   
     const langChangeObserver = new MutationObserver(() => {
       loadAuthData();
     });

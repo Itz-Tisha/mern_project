962089e65aad5b7030a60a02d3e699ff2bb9dc09
Tisha <159291720+Itz-Tisha@users.noreply.github.com>
2025-11-17 22:34:42 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,15 +6,11 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const session = require('express-session');
 const passport = require('passport');
-//const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const WebUser = require('./models/webuser');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const dbpath = process.env.DB_PATH;
-
-// app.set('view engine', 'ejs');
-// app.set('views', './views');
 
 app.use(cors());
 app.use('/uploads', express.static('uploads')); 
@@ -34,46 +30,6 @@ app.use(session({
     store: store,
 }));
 
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// passport.serializeUser((user, done) => done(null, user.id));
-// passport.deserializeUser((id, done) => {
-//     WebUser.findById(id).then(user => done(null, user));
-// });
-
-// passport.use(new GoogleStrategy({
-//     clientID: process.env.GOOGLE_CLIENT_ID,
-//     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//     callbackURL: "http://localhost:9579/auth/google/callback"
-// }, async (accessToken, refreshToken, profile, done) => {
-//     try {
-//         let user = await WebUser.findOne({ googleId: profile.id });
-//         if (!user) {
-//             user = await WebUser.create({
-//                 googleId: profile.id,
-//                 email: profile.emails[0].value,
-//                 name: profile.displayName,
-//                 usertype: 'farmer',
-//                 password: ''
-//             });
-//         }
-//         done(null, user);
-//     } catch (err) {
-//         done(err, null);
-//     }
-// }));
-
-// app.get('/auth/google',
-//     passport.authenticate('google', { scope: ['profile', 'email'] })
-// );
-
-// app.get('/auth/google/callback',
-//     passport.authenticate('google', { failureRedirect: '/login' }),
-//     (req, res) => {
-//         res.redirect('/');
-//     }
-// );
 
 app.use(router);
 

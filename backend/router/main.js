574e89a0ff03e6d9c055b1a9036892e92e03c9a2
Tisha <159291720+Itz-Tisha@users.noreply.github.com>
2025-11-19 15:34:30 +0530

    Remove unused upload middleware import

--- backend/router/main.js
+++ backend/router/main.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const upload = require('../uploadMiddleware');
+
 const { 
   home, sign, login, postq, profile, postart, viewarticle, displayq, answerq, 
   viewans, editprofile,  postcomments, getcomments, likearticle, 
